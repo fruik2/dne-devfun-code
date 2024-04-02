eac231fb6bfd5cd12301e3894e2fc2dc7f12c25d
TJ <fruik2@gmail.com>
2024-04-02 16:34:17 -0400

    git commit -m "Test Commit Hello world"

---
+++ intro-python/print world.py
@@ -0,0 +1,17 @@
+#Below is the code to print Hello World
+print('Hello World!')
+
+username = input('Please enter your name: ')
+age = int(input('Please enter your age: '))
+print(username)
+print(age)
+print(type(username))
+print(type(age))
+
+
+
+
+a = 5
+b = 2
+c = a + b
+print('Addition is', c)
